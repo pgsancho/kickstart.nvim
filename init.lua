7cbcec4a94ff3d8d1bc3d86f78de6442d09366ba
Pablo García <pgarcia@fluendo.com>
2026-01-12 10:11:23 +0100

    change colorcheme to 'gruvbox'

--- init.lua
+++ init.lua
@@ -881,20 +881,25 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    -- 'aktersnurra/no-clown-fiesta.nvim',
+    -- 'folke/tokyonight.nvim',
+    -- 'rebelot/kanagawa.nvim',
+    -- 'EdenEast/nightfox.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      require('gruvbox').setup {
+        terminal_colors = true,
+        inverse = true,
+        contrast = 'hard',
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
 
